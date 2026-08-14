bb4cfc92089385c5cabe6cc1c0b3ac541dfbf011
Gabriel Aparecido <contato.gabrielaparecido36@gmail.com>
2026-08-13 22:03:42 -0300

    create piece and board class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+import boardgame.Board;
 import boardgame.Position;
 
 void main(String[] args) {
-    Position position = new Position(1, 2);
-    System.out.println(position);
+    Board board = new Board(8, 8);
+    
 }
