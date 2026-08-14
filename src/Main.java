3936adeccb1dd719d7d1914537a130d1477f4fb9
Gabriel Aparecido <contato.gabrielaparecido36@gmail.com>
2026-08-13 21:57:20 -0300

    create position class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
-void main() {
-    IO.println("Hello World");
+import boardgame.Position;
+
+void main(String[] args) {
+    Position position = new Position(1, 2);
+    System.out.println(position);
 }
